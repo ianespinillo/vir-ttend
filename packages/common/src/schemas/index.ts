4d53e87b8e0404e061ed004dc6a8638dcaf5a867
Ian Espinillo <ianespinillo@gmail.com>
2026-08-23 22:23:37 -0300

    feat(common): announcement contract, route and form schema

--- packages/common/src/schemas/index.ts
+++ packages/common/src/schemas/index.ts
@@ -1,4 +1,5 @@
 export * from './auth.schema.js';
+export * from './announcement.schema.js';
 export * from './student.schema.js';
 export * from './attendance.schema.js';
 export * from './academic-year.schema.js';
